f02d3ba943fbac60a8ff1ea14fb86cc51348840b
anshgs <anshsharma@gmail.com>
2022-02-22 23:45:59 -0600

    creating new branch to mess around tensorflow with

--- client/src/components/style/Styler.js
+++ client/src/components/style/Styler.js
@@ -6,11 +6,11 @@ const Styler = () => {
   const styleNet = tf.loadGraphModel('saved_model_style_js/model.json');
   const transformNet = tf.loadGraphModel('saved_model_transformer_separable_js/model.json');
 
-  // const ratio = 0.5
-  // const firstStyle = styleNet.predict(tf.browser.fromPixels(styleImg).toFloat().div(tf.scalar(255)).expandDims());
-  // const secondStyle = styleNet.predict(tf.browser.fromPixels(styleImg).toFloat().div(tf.scalar(255)).expandDims());
-  // const bottleneck = ratio*firstStyle + (1-ratio) * secondStyle;
-  // const stylized = transformNet.predict([tf.browser.fromPixels(contentImg).toFloat().div(tf.scalar(255)).expandDims(), bottleneck]).squeeze();
+  const ratio = 0.5
+  const firstStyle = styleNet.predict(tf.browser.fromPixels(styleImg).toFloat().div(tf.scalar(255)).expandDims());
+  const secondStyle = styleNet.predict(tf.browser.fromPixels(styleImg).toFloat().div(tf.scalar(255)).expandDims());
+  const bottleneck = ratio*firstStyle + (1-ratio) * secondStyle;
+  const stylized = transformNet.predict([tf.browser.fromPixels(contentImg).toFloat().div(tf.scalar(255)).expandDims(), bottleneck]).squeeze();
 
   return (
     <main style={{ padding: "1rem 0" }}>
