52f98e436126ca79e7931a30040659daf2f0fde7
jliu179 <jliu179@illinois.edu>
2022-04-01 00:51:14 -0500

    add support for hdris

--- client/src/components/edit/UI.js
+++ client/src/components/edit/UI.js
@@ -3,6 +3,7 @@ import Box from '@material-ui/core/Box'
 import { UploadButton } from "./UploadButton";
 import { DeleteButton } from "./DeleteButton";
 import { SceneList } from './SceneList';
+import { EnvironmentSelector } from './EnvironmentSelector';
 import { TransformOptions } from './TransformOptions';
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
@@ -14,12 +15,13 @@ const UI = (props) => {
   }
 
   return (
-    <Box style={{position: 'fixed', zIndex: 999}}>
+    <Box style={{position: 'fixed', zIndex: 999, background: 'rgba(255, 255, 255, 0.8)'}}>
       <UploadButton objects={props.objects} setObjects={props.setObjects}/>
       <DeleteButton objects={props.objects} setObjects={props.setObjects} selectedObject={props.selectedObject} setSelectedObject={props.setSelectedObject}/>
       <FormControlLabel control={<Checkbox checked={props.toggleGrid} onChange={handleToggleGrid} />} label="Display Grid" />
       <SceneList objects={props.objects} selected={props.selectedObject} setSelected={props.setSelectedObject}/>
       <TransformOptions transformMode={props.transformMode} setTransformMode={props.setTransformMode}/>
+      <EnvironmentSelector environment={props.environment} setEnvironment={props.setEnvironment}/>
     </Box>
   );
 };
