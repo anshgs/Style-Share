4d3e78b9b2bf4b5eea323ba2f8794eebe94e69d6
jliu179 <jliu179@illinois.edu>
2022-04-08 12:26:22 -0500

    add option to upload style or content

--- client/src/components/edit/SaveButton.js
+++ client/src/components/edit/SaveButton.js
@@ -1,18 +1,32 @@
 import React, { useRef } from 'react';
 import Button from '@mui/material/Button';
 
-const SaveButton = (props) => {
+const SaveStyleButton = (props) => {
   // callback to delete object
   const handleOnClick = (e) => {
     // only add back objects that do not match the selected object
-    props.saveCanvas();
+    props.saveCanvas(true);
   }
 
   return (
     <Button variant="contained" component="label" onClick={handleOnClick}>
-      Save Scene
+      Save Scene as Style
     </Button>
   );
 }
 
-export { SaveButton }
+const SaveContentButton = (props) => {
+  // callback to delete object
+  const handleOnClick = (e) => {
+    // only add back objects that do not match the selected object
+    props.saveCanvas(false);
+  }
+
+  return (
+    <Button variant="contained" component="label" onClick={handleOnClick}>
+      Save Scene as Content
+    </Button>
+  );
+}
+
+export { SaveStyleButton, SaveContentButton }
--- client/src/components/edit/UI.js
+++ client/src/components/edit/UI.js
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import Box from '@material-ui/core/Box'
 import { UploadButton } from "./UploadButton";
 import { DeleteButton } from "./DeleteButton";
-import { SaveButton } from "./SaveButton";
+import { SaveStyleButton, SaveContentButton } from "./SaveButton";
 import { SceneList } from './SceneList';
 import { EnvironmentSelector } from './EnvironmentSelector';
 import { TransformOptions } from './TransformOptions';
@@ -23,7 +23,8 @@ const UI = (props) => {
       <SceneList objects={props.objects} selected={props.selectedObject} setSelected={props.setSelectedObject}/>
       <TransformOptions transformMode={props.transformMode} setTransformMode={props.setTransformMode}/>
       <EnvironmentSelector environment={props.environment} setEnvironment={props.setEnvironment}/>
-      <SaveButton saveCanvas={props.saveCanvas}/>
+      <SaveStyleButton saveCanvas={props.saveCanvas}/>
+      <SaveContentButton saveCanvas={props.saveCanvas}/>
     </Box>
   );
 };
