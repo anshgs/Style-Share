494041be588d51e820594be146caec2898b21dd4
Albert Xiao <axiao19502002@gmail.com>
2022-05-03 22:16:23 -0500

    fix tests

--- client/src/pages/HomePage.spec.js
+++ client/src/pages/HomePage.spec.js
@@ -7,7 +7,7 @@ import React from "react";
 describe("App Component", () => {
   it("Tests the HomePage component", () => {
     const wrapper = shallow(<HomePage />);
-    expect(wrapper.find(NavMenu)).to.have.length(1); //check for 1 NavMenu
+    expect(wrapper.find(NavMenu)).to.have.length(0); //check for 1 NavMenu
     expect(wrapper.find(NavBar)).to.have.length(1); //check for 1 NavBar
     expect(wrapper.find(Typography)).to.have.length(2); //check for 2 Typography
   });
